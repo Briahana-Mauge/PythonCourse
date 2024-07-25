fff25c1b097c57a3e889d549cf2142453a11374b
Briahana Maugé <52139668+Briahana-Mauge@users.noreply.github.com>
2024-07-25 17:44:43 -0400

    print the first few strings in python ever

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+# anything in quotes is a string 
+print("Hello World")
+print('o----')
+print(' ||||')
+#the following statement is an expression where we are multiplying the string
+print('*' * 10)  #expected: **********
