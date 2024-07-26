60a26c76db130ac228822d0b6b81f402758bb6eb
Briahana Maugé <52139668+Briahana-Mauge@users.noreply.github.com>
2024-07-25 22:35:48 -0400

    add count method

--- Notes/Strings.py
+++ Notes/Strings.py
@@ -67,10 +67,11 @@ print(len(course)) # -> 20
 
 
 #  STRING METHODS
-# .upper(str) - converts a str to uppercase letters. DOES NOT MODIFY ORIGINAL STRING
-# .lower(str) - converts a str to loowercase letters. DOES NOT MODIFY ORIGINAL STRING
+# .upper() - converts a str to uppercase letters. DOES NOT MODIFY ORIGINAL STRING
+# .lower() - converts a str to loowercase letters. DOES NOT MODIFY ORIGINAL STRING
 # .find(str) - returns the index when str is FIRST spotted. returns -1 when not found. CASE SENSATIVE
 # .replace(old str, new str) - replaces the first argument with the second argument in all instances. CASE SENSATIVE
+# .count('str') - counts the number of occurrences of one string in another.
 # 'str' in string_variable - checks to see if str exsits in the string variable and returns a bool. CASE SENSATIVE
 # .split(char in quotes) - splits the string into an array based on the character in the argument
 # 'char in quotes'.join(string_variable) - joins an array into a string based on the character in quotes
@@ -84,6 +85,7 @@ print(course.find('Beginners')) # -> 11 the word starts at index 11
 print(course.replace('Beginners', 'Absolute Beginners')) # -> Python for Absolute Beginners
 print(course.replace('beginners', 'Absolute Beginners')) # -> Python for Beginners (beginners was not found so it didn't replace anything)
 print(course.replace('n', 'm')) # -> Pythom for Begimmers
+print(course.count('for')) # -> 1
 print('Python' in course) # -> True
 print('FOR' in course) # -> False
 print(course.split(' ')) # -> ['Python', 'for', 'Beginners']
