5d97c3cb8a40069b6dfe3b7fbb68e96d4eb48c22
Briahana Maugé <52139668+Briahana-Mauge@users.noreply.github.com>
2024-07-25 20:35:32 -0400

    remove notes from first level

--- HelloWorld.py
+++
@@ -1,6 +0,0 @@
-# anything in quotes is a string 
-print("Hello World")
-print('o----')
-print(' ||||')
-#the following statement is an expression where we are multiplying the string
-print('*' * 10)  #expected: **********
